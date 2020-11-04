4a76e8d8d5d43841a5ed23c9a047d823d23ff922
Jorrit <458871@student.fontys.nl>
2020-11-04 14:31:32 +0100

    Hoofdmenu gemaakt met buttons en script om naar scenes te gaan.

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{ 
+    private void Awake()
+    {
+        //Zorgt ervoor dat de Level Manager niet verwijderd wordt zodra een nieuwe scene/level wordt geladen!
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void LaadScene(int sceneIndex)
+    {
+        //Laadt de scene via een index. De index van een scene kan je vinden bij Build Settings in Unity.
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void LaadVolgendeScene()
+    {
+        //Pakt de huidige scene en kijkt wat de index van die scene is. Daarna laadt hij de scene door +1 te doen bij de huidige index.
+        int huidigeSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(huidigeSceneIndex + 1);
+    }
+
+    public void SpelAfsluiten()
+    {
+        Application.Quit();
+    }
+}
