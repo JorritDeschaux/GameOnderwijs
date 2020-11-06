1a3bf39226ede45e6970a81b4b53802daa2bf9f1
Gardevour <gardevoured@gmail.com>
2020-11-06 10:02:45 +0100

    Pushed keypad controls to main

---
+++ Assets/Ardity/Scripts/Samples/SampleUserPolling_ReadWrite.cs
@@ -0,0 +1,123 @@
+/**
+ * Ardity (Serial Communication for Arduino + Unity)
+ * Author: Daniel Wilches <dwilches@gmail.com>
+ *
+ * This work is released under the Creative Commons Attributions license.
+ * https://creativecommons.org/licenses/by/2.0/
+ */
+
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+/**
+ * Sample for reading using polling by yourself, and writing too.
+ */
+public class SampleUserPolling_ReadWrite : MonoBehaviour
+{
+    public SerialController serialController;
+    public LevelManager levelManager;
+
+    // Initialization
+    void Start()
+    {
+        serialController = GameObject.Find("SerialController").GetComponent<SerialController>();
+    }
+
+    // Executed each frame
+    void Update()
+    {
+        //---------------------------------------------------------------------
+        // Receive data
+        //---------------------------------------------------------------------
+
+        string message = serialController.ReadSerialMessage();
+
+        if (message == null)
+            return;
+
+        // Check if the message is plain data or a connect/disconnect event.
+        if (ReferenceEquals(message, SerialController.SERIAL_DEVICE_CONNECTED))
+            Debug.Log("Connection established");
+        else if (ReferenceEquals(message, SerialController.SERIAL_DEVICE_DISCONNECTED))
+            Debug.Log("Connection attempt failed or disconnection detected");
+        else if (message == "1")
+        {
+            if(SceneManager.GetActiveScene().buildIndex == 0)
+            {
+                levelManager.LaadScene(2);
+            }
+        }
+        else if (message == "2")
+        {
+            if (SceneManager.GetActiveScene().buildIndex == 0)
+            {
+                levelManager.LaadVolgendeScene();
+            }           
+        }
+        else if (message == "3")
+        {
+            if (SceneManager.GetActiveScene().buildIndex == 0)
+            {
+                levelManager.SpelAfsluiten();
+            }
+            if (SceneManager.GetActiveScene().buildIndex == 1)
+            {
+                levelManager.LaadScene(0);
+            }
+
+        }
+        else if (message == "A")
+        {
+            Debug.Log("Key A pressed");
+        }
+        else if (message == "4")
+        {
+            Debug.Log("Key 4 pressed");
+        }
+        else if (message == "5")
+        {
+            Debug.Log("Key 5 pressed");
+        }
+        else if (message == "6")
+        {
+            Debug.Log("Key 6 pressed");
+        }
+        else if (message == "B")
+        {
+            Debug.Log("Key B pressed");
+        }
+        else if (message == "7")
+        {
+            Debug.Log("Key 7 pressed");
+        }
+        else if (message == "8")
+        {
+            Debug.Log("Key 8 pressed");
+        }
+        else if (message == "9")
+        {
+            Debug.Log("Key 9 pressed");
+        }
+        else if (message == "C")
+        {
+            Debug.Log("Key C pressed");
+        }
+        else if (message == "*")
+        {
+            Debug.Log("Key * pressed");
+        }
+        else if (message == "0")
+        {
+            Debug.Log("Key 0 pressed");
+        }
+        else if (message == "#")
+        {
+            Debug.Log("Key # pressed");
+        }
+        else if (message == "D")
+        {
+            Debug.Log("Key D pressed");
+        }
+    }
+}
